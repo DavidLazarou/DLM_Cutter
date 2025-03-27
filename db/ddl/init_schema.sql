0f863658e84209a2290d3ddf2f39d2afe2ee0c39
Dave <davelazarou1000@gmail.com>
2025-03-27 02:08:22 +0100

    🗃️ Structure: Move DB DDL to dedicated folder

---
+++ db/ddl/init_schema.sql
@@ -0,0 +1,11 @@
+-- FRED data
+CREATE TABLE fred_series (...);
+
+-- Stock prices
+CREATE TABLE stock_prices (...);
+
+-- Indicators
+CREATE TABLE stock_indicators (...);
+
+-- Rolling correlations
+CREATE TABLE rolling_correlations (...);
---
+++ db/ddl/schema_v01.sql
@@ -0,0 +1,1 @@
+-- CREATE TABLE stock_prices (...);
